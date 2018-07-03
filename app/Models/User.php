575e38a95ac3cfc319c22dacee46adc198b1babb
ShiYe <2279985550@qq.com>
2018-07-03 14:41:28 +0000

    edit Modes/User

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use App\Notifications\ResetPassword;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Support\Facades\Auth;
 
 class User extends Authenticatable
 {
